8f8cefde12d09bc0c4f62c3e100213dd3892981b
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2025-02-17 21:59:52 +0800

    first

---
+++ src/main/java/com/learn/flight_time_calculator/FlightTimeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.learn.flight_time_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTimeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTimeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/flight_time_calculator/FlightTimeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.flight_time_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTimeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
